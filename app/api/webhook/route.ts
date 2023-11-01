74fe40bff5ade17e7e8077851030baa7fee066a3
Diego <diegofreeman78@gmail.com>
2023-11-01 14:50:09 +0100

    ui updated

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -104,39 +104,138 @@
 // // Resource: https://docs.svix.com/receiving/verifying-payloads/why
 // // It's a good practice to verify webhooks. Above article shows why we should do it
 
-import { NextResponse } from 'next/server';
-import { createUser, getUser } from '@/lib/actions/member';
-import { auth } from '@clerk/nextjs';
+// import { NextResponse } from 'next/server';
+// import { createUser, getUser } from '@/lib/actions/member';
+// import { auth } from '@clerk/nextjs';
 
-// Resource: https://clerk.com/docs/integration/webhooks#supported-events
-// Above document lists the supported events
-type EventType = 'user.created' | 'session.created';
+// // Resource: https://clerk.com/docs/integration/webhooks#supported-events
+// // Above document lists the supported events
+// type EventType = 'user.created' | 'session.created';
 
-import type { IncomingHttpHeaders } from 'http';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { WebhookRequiredHeaders } from 'svix';
-import type { WebhookEvent } from '@clerk/nextjs/server';
+// import type { IncomingHttpHeaders } from 'http';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import type { WebhookRequiredHeaders } from 'svix';
+// import type { WebhookEvent } from '@clerk/nextjs/server';
+// import { Webhook } from 'svix';
+
+// const webhookSecret: string = process.env.NEXT_CLERK_WEBHOOK_SECRET!;
+
+// export default async function handler(
+//   req: NextApiRequestWithSvixRequiredHeaders,
+//   res: NextApiResponse
+// ) {
+//   const payload = JSON.stringify(req.body);
+//   const headers = req.headers;
+//   // Create a new Webhook instance with your webhook secret
+//   const wh = new Webhook(webhookSecret);
+
+//   let evt: WebhookEvent;
+//   try {
+//     // Verify the webhook payload and headers
+//     evt = wh.verify(payload, headers) as WebhookEvent;
+//   } catch (_) {
+//     // If the verification fails, return a 400 error
+//     return res.status(400).json({});
+//   }
+
+//   const eventType = evt.type;
+//   if (eventType === 'user.created') {
+//     const { id, image_url, first_name, last_name } = evt?.data ?? {};
+
+//     try {
+//       // @ts-ignore
+//       await createUser({
+//         userId: id as string,
+//         name: `${first_name} ${last_name}`,
+//         role: 'user',
+//         avatarUrl: image_url as string,
+//       });
+
+//       return NextResponse.json({ message: 'User created' }, { status: 201 });
+//     } catch (err) {
+//       console.log(err);
+//       return NextResponse.json(
+//         { message: 'Internal Server Error' },
+//         { status: 500 }
+//       );
+//     }
+//   }
+
+//   if (eventType === 'session.created') {
+//     try {
+//       const { user_id } = evt?.data;
+
+//       // @ts-ignore
+//       await getUser(user_id);
+
+//       return NextResponse.json({ message: 'User found' }, { status: 200 });
+//     } catch (err) {
+//       console.log(err);
+
+//       return NextResponse.json(
+//         { message: 'Internal Server Error' },
+//         { status: 500 }
+//       );
+//     }
+//   }
+// }
+
+// type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+//   headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+// };
+
 import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-const webhookSecret: string = process.env.NEXT_CLERK_WEBHOOK_SECRET!;
+export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
-export default async function handler(
-  req: NextApiRequestWithSvixRequiredHeaders,
-  res: NextApiResponse
-) {
-  const payload = JSON.stringify(req.body);
-  const headers = req.headers;
-  // Create a new Webhook instance with your webhook secret
-  const wh = new Webhook(webhookSecret);
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+    );
+  }
+
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400,
+    });
+  }
+
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  // Create a new SVIX instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
+
+  // Verify the payload with the headers
   try {
-    // Verify the webhook payload and headers
-    evt = wh.verify(payload, headers) as WebhookEvent;
-  } catch (_) {
-    // If the verification fails, return a 400 error
-    return res.status(400).json({});
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400,
+    });
   }
+
+  // Get the ID and type
 
   const eventType = evt.type;
   if (eventType === 'user.created') {
@@ -178,8 +277,6 @@ export default async function handler(
       );
     }
   }
-}
 
-type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-};
+  return new Response('', { status: 201 });
+}
--- lib/actions/comments.ts
+++ lib/actions/comments.ts
@@ -4,6 +4,7 @@ import { auth } from '@clerk/nextjs';
 import { connectToDB } from '../mongoose';
 import Post from '../model/user';
 import Comment from '../model/comment';
+import { NextResponse } from 'next/server';
 
 export const createComment = async (
   postId: string,
@@ -25,6 +26,39 @@ export const createComment = async (
     });
 
     await comment.save();
+  } catch (error) {
+    console.log(error);
+
+    return { message: 'Failed to create comment' };
+  }
+};
+export const replyToComment = async (
+  replyToId: string,
+  content: string,
+  ownerId: string
+) => {
+  const { user } = auth();
+  if (!user) return { message: 'User not authenticated' };
+  connectToDB();
+
+  try {
+    const chiefComment = await Comment.findOne({
+      _id: replyToId,
+
+      chiefComment: true,
+    });
+    if (!chiefComment) return { message: 'Comment not found' };
+    const replyComment = new Comment({
+      content,
+      owner: ownerId,
+      replyTo: replyToId,
+    });
+
+    if (chiefComment.replies)
+      chiefComment.replies = [...chiefComment.replies, replyComment._id];
+    await chiefComment.save();
+    await replyComment.save();
+    return replyComment;
   } catch (error) {
     console.log(error);
 
