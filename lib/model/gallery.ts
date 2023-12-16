75ca98ea9d1d6deeef9f6d227e1ae38c01d2d2cc
Diegofreema <104385367+Diegofreema@users.noreply.github.com>
2023-12-16 10:37:21 +0100

    Update gallery.ts

--- lib/model/gallery.ts
+++ lib/model/gallery.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const GallerySchema = new mongoose.Schema({
   imgUrl: { type: String, required: true },
+   createdAt: { type: Date, default: Date.now }
 });
 
 const Gallery =
