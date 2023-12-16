6fcc35fa73578c79844b89410dd81f5bfb5f264d
Diegofreema <104385367+Diegofreema@users.noreply.github.com>
2023-12-16 10:37:36 +0100

    Update video.ts

--- lib/model/video.ts
+++ lib/model/video.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const VideoSchema = new mongoose.Schema({
   videoUrl: { type: String, required: true },
+   createdAt: { type: Date, default: Date.now }
 });
 
 const Video = mongoose.models.Video || mongoose.model('Video', VideoSchema);
