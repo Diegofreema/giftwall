7251a8a76f5c47da3bbd68076826825c300196ee
Diegofreema <104385367+Diegofreema@users.noreply.github.com>
2024-03-01 07:00:43 +0100

    Update Button.tsx

--- Ui/Button.tsx
+++ Ui/Button.tsx
@@ -4,10 +4,12 @@ import React from 'react';
 interface Props {
   children: React.ReactNode;
   rightSection?: React.ReactNode;
+  onClick?: () => void;
 }
-const MyButton = ({ children, rightSection }: Props) => {
+const MyButton = ({ children, rightSection, onClick }: Props) => {
   return (
     <Button
+      onClick={onClick}
       className="!bg-yellow-400 z-30 hover:!text-black transition group rounded-full "
       rightSection={rightSection}
     >
